2721644ff498878e978e472e513e64eb5205f246
koheishingaiHQ <koheishingai+hq@gmail.com>
2017-11-01 16:25:49 +0900

    Fix header top

--- src/container/Nav.js
+++ src/container/Nav.js
@@ -24,10 +24,11 @@ class Nav extends Component {
     var menu = [];
     var data = [
       //{name: path.detail.split('/').join(''), path: path.detail, class: 'detail'},
+      {name: "view", path: "https://koheishingaihq.github.io/cmsy-for-uiine-tokyo/", class: ''},
       {name: "recently", path: path.list, class: 'list'}
     ];
     for(var i in data){
-      menu.push(<Link to={data[i].path} key={data[i].name}><li className={data[i].class}>{data[i].name}</li></Link>);
+      menu.push(<a href={(data[i].path.indexOf("http") !== -1) ? data[i].path : "#/" + data[i].path} target={(data[i].path.indexOf("http") !== -1) ? "_blank" : ""} key={data[i].name}><li className={data[i].class}>{data[i].name}</li></a>);
     }
     this.setState({ menu : menu });
   }
